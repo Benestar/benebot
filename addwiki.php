0428367e187f022f27e5e7739c8c81b7f3a2b275
Bene <benestar.wikimedia@gmail.com>
2015-09-08 17:50:40 +0200

    Add first script UpdateBadges

--- addwiki.php
+++ addwiki.php
@@ -4,5 +4,21 @@
  * Register commands for addwiki.
  */
 
-// $awwCommands[] = new BeneBot\FancyFormatter();
+$dataValueDeserializer = new DataValues\Deserializers\DataValueDeserializer( array(
+	'boolean' => 'DataValues\BooleanValue',
+	'number' => 'DataValues\NumberValue',
+	'string' => 'DataValues\StringValue',
+	'unknown' => 'DataValues\UnknownValue',
+	'globecoordinate' => 'DataValues\Geo\Values\GlobeCoordinateValue',
+	'monolingualtext' => 'DataValues\MonolingualTextValue',
+	'multilingualtext' => 'DataValues\MultilingualTextValue',
+	'quantity' => 'DataValues\QuantityValue',
+	'time' => 'DataValues\TimeValue',
+	'wikibase-entityid' => 'Wikibase\DataModel\Entity\EntityIdValue',
+) );
 
+$GLOBALS['awwCommands'][] = function( Mediawiki\Bot\Config\AppConfig $appConfig ) use ( $dataValueDeserializer ) {
+	return array(
+		new BeneBot\UpdateBadges( $appConfig, $dataValueDeserializer )
+	);
+};
---
+++ src/UpdateBadges.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace BeneBot;
+
+use DataValues\Serializers\DataValueSerializer;
+use Deserializers\Deserializer;
+use Mediawiki\Api\ApiUser;
+use Mediawiki\Api\MediawikiApi;
+use Mediawiki\Bot\Config\AppConfig;
+use Mediawiki\DataModel\EditInfo;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Wikibase\Api\WikibaseFactory;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\SiteLink;
+
+/**
+ * Update badges based on Wikipedia categories on Wikidata.
+ *
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class UpdateBadges extends Command {
+
+	/**
+	 * @var AppConfig
+	 */
+	private $appConfig;
+
+	/**
+	 * @var Deserializer
+	 */
+	private $dataValueDeserializer;
+
+	public function __construct( AppConfig $appConfig, Deserializer $dataValueDeserializer ) {
+		$this->appConfig = $appConfig;
+		$this->dataValueDeserializer = $dataValueDeserializer;
+
+		parent::__construct( null );
+	}
+
+	protected function configure() {
+		$defaultUser = $this->appConfig->get( 'defaults.user' );
+		$defaultWiki = $this->appConfig->get( 'defaults.wiki' );
+		$defaultRepo = $this->appConfig->get( 'defaults.repo' );
+
+		$this->setName( 'task:update-badges' )
+			->setDescription( 'Update badges based on Wikipedia categories on Wikidata' )
+			->addOption(
+				'user',
+				null,
+				( $defaultUser === null ? InputOption::VALUE_REQUIRED : InputOption::VALUE_OPTIONAL ),
+				'The configured user to use',
+				$defaultUser
+			)
+			->addOption(
+				'wiki',
+				null,
+				( $defaultWiki === null ? InputOption::VALUE_REQUIRED : InputOption::VALUE_OPTIONAL ),
+				'The configured wiki to use',
+				$defaultWiki
+			)
+			->addOption(
+				'repo',
+				null,
+				( $defaultRepo === null ? InputOption::VALUE_REQUIRED : InputOption::VALUE_OPTIONAL ),
+				'The Wikibase repository to use',
+				$defaultRepo
+			)
+			->addOption(
+				'bot',
+				null,
+				InputOption::VALUE_OPTIONAL,
+				'Mark edits as bot',
+				true
+			)
+			->addOption(
+				'summary',
+				null,
+				InputOption::VALUE_OPTIONAL,
+				'Override the default edit summary',
+				'Bot: Adding badge [[$badgeId]] for site $wiki based on Category:$category'
+			);
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output ) {
+		$wiki = $input->getOption( 'wiki' );
+		$user = $input->getOption( 'user' );
+		$repo = $input->getOption( 'repo' );
+
+		$userDetails = $this->appConfig->get( 'users.' . $user );
+		$wikiDetails = $this->appConfig->get( 'wikis.' . $wiki );
+		$repoDetails = $this->appConfig->get( 'wikis.' . $repo );
+		$databaseDetails = $this->appConfig->get( 'users.database' );
+
+		if( $userDetails === null ) {
+			throw new RuntimeException( 'User not found in config' );
+		}
+
+		if( $wikiDetails === null ) {
+			throw new RuntimeException( 'Wiki not found in config' );
+		}
+
+		if( $repoDetails === null ) {
+			throw new RuntimeException( 'Repo not found in config' );
+		}
+
+		$api = new MediawikiApi( $wikiDetails['url'] );
+		$repoApi = new MediawikiApi( $repoDetails['url'] );
+		$loggedIn = $repoApi->login( new ApiUser( $userDetails['username'], $userDetails['password'] ) );
+		$db = new \mysqli( $wiki . '.labsdb', $databaseDetails['username'], $databaseDetails['password'], $wiki . '_p' );
+
+		if( !$loggedIn || $db->connect_error ) {
+			$output->writeln( 'Failed to log in' );
+			return -1;
+		}
+
+		$wikibaseFactory = new WikibaseFactory( $repoApi, $this->dataValueDeserializer, new DataValueSerializer() );
+		$revisionsGetter = $wikibaseFactory->newRevisionGetter();
+		$siteLinkSetter = $wikibaseFactory->newSiteLinkSetter();
+
+		$badgeId = new ItemId( $input->getOption( 'badge' ) );
+		$editInfo = new EditInfo(
+			$this->getSummary( $input->getOption( 'summary' ), $input->getOption( 'category' ), $wiki, $badgeId->getSerialization() ),
+			false,
+			$input->getOption( 'bot' )
+		);
+
+		$results = $db->query(
+			'SELECT page_title FROM categorylinks
+			JOIN page ON page_id = cl_from
+			WHERE cl_to = "' . $db->escape_string( $input->getOption( 'category' ) ) . '"
+			AND page_namespace = 0'
+		);
+
+		if ( $results ) {
+			while ( $row = $results->fetch_assoc() ) {
+				/** @var Item $item */
+				$item = $revisionsGetter->getFromSiteAndTitle( $wiki, $row['page_title'] )->getContent()->getData();
+				$badges = $item->getSiteLinkList()->getBySiteId( $wiki )->getBadges();
+				$badges[] = $badgeId;
+				$success = $siteLinkSetter->set(
+					new SiteLink( $wiki, $row['page_title'], $badges ),
+					new SiteLink( $wiki, $row['page_title'] )
+					//$editInfo
+				);
+
+				if ( $success ) {
+					$output->writeln( "Added badge for $wiki:{$row['page_title']}" );
+				} else {
+					$output->writeln( "Failed to add badge for $wiki:{$row['page_title']}" );
+				}
+			}
+		}
+
+		return null;
+	}
+
+	private function getSummary( $rawSummary, $category, $wiki, $badgeId ) {
+		return strtr(
+			$rawSummary,
+			array(
+				'$category' => $category,
+				'$wiki' => $wiki,
+				'$badgeId' => $badgeId
+			)
+		);
+	}
+
+}
